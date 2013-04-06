51c5853bab79260fa515d15d3ac3ba288bb41548
Ryan Hirsch <Ryan.Hirsch@gmail.com>
2013-04-06 18:06:10 -0500

    Add about content and link

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,11 @@
 /*global define */
 define(['ember'], function (Ember) {
-    'use strict';
-    var App = Ember.Application.create();
-    return App;
+  'use strict';
+  var App = Ember.Application.create();
+
+  App.Router.map(function () {
+    this.resource('about');
+  })
+
+  return App;
 });
